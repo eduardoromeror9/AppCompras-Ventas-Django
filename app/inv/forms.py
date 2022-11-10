9758b241df63411d1b242bb3e217fb296b8b75b0
Eduardo Romero <eduardoromeror9@outlook.com>
2022-11-10 06:17:37 -0300

    Actualizar list y unidades de medida

--- app/inv/forms.py
+++ app/inv/forms.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from django import forms
-from .models import Categoria, SubCategoria
+from .models import Categoria, SubCategoria, Marca
 
 
 class CategoriaForm(forms.ModelForm):
@@ -35,3 +35,18 @@ class SubCategoriaForm(forms.ModelForm):
                 'class': 'form-control'
             })
         self.fields['categoria'].empty_label = 'Seleccione Categoria'
+        
+
+class MarcaForm(forms.ModelForm):
+    class Meta:
+        model = Marca
+        fields = ['descripcion', 'estado']
+        labels = {'descripcion': 'Descripcion de la Marca', 'estado': 'Estado'}
+        widget = {'descripcion': forms.TextInput}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
--- app/inv/models.py
+++ app/inv/models.py
@@ -38,3 +38,21 @@ class SubCategoria(ClaseModelo):
     class Meta:
         verbose_name_plural = 'Subcategorias'
         unique_together = ('categoria', 'descripcion')
+        
+
+class Marca(ClaseModelo):
+    descripcion = models.CharField(
+        max_length = 100,
+        help_text = 'Descripcion de la Marca',
+        unique = True
+    )
+    
+    def __str__(self):
+        return f'{self.descripcion}'
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Marca, self).save()
+        
+    class Meta:
+        verbose_name_plural = 'Marcas'
